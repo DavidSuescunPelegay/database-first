f45f6bf0a778289bfefe277697e40355cb172ae0
David Suescun Pelegay <dsuescun96@gmail.com>
2017-10-18 21:23:48 +0200

    Code upload

---
+++ DbFirst/DbFirst/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var dbContext = new PlutoDbContext();
+
+            dbContext.GetAuthorCourses();
+
+            var courses = dbContext.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+            */
+
+            var course=new Course();
+            course.Level = Level.Beginner; // 1
+        }
+    }
+}
